d39aa176590ea092d7c90922b39aa4a971d006e4
BillUnboxedCrate <bram.baert@hotmail.com>
2014-11-05 16:51:58 +0100

    Validatie voor kamp toegevoegd

--- webapp/server/api/kamp/kamp.model.js
+++ webapp/server/api/kamp/kamp.model.js
@@ -18,4 +18,14 @@ var KampSchema = new Schema({
     inschrijvingen: [{type: Schema.ObjectId, ref: "InschrijvingSchema"}]
 });
 
+KampSchema.path('beschrijving').required(true, "Beschrijving mag niet leeg zijn");
+KampSchema.path('contact').required(true, "Contact mag niet leeg zijn");
+KampSchema.path('eindDatum').required(true, "Einddatum mag niet leeg zijn");
+KampSchema.path('startDatum').required(true, "Begindatum mag niet leeg zijn");
+KampSchema.path('leeftijdsCatDoelgroep').required(true, "Leeftijdscategorie mag niet leeg zijn");
+KampSchema.path('locatie').required(true, "Locatie mag niet leeg zijn");
+KampSchema.path('naam').required(true, "Naam mag niet leeg zijn");
+KampSchema.path('prijs').required(true, "Prijs mag niet leeg zijn");
+KampSchema.path('vervoer').required(true, "Vervoer mag niet leeg zijn");
+
 module.exports = mongoose.model('Kamp', KampSchema);
