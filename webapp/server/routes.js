3645e34c6df7b588dcf4d04523c2af4b69e718fd
BillUnboxedCrate <bram.baert@hotmail.com>
2014-11-07 15:03:47 +0100

    Toevoegen email voor back-end Verwijderen van onnodige sockets

--- webapp/server/routes.js
+++ webapp/server/routes.js
@@ -18,6 +18,7 @@ module.exports = function(app) {
   app.use('/api/nieuwsitems', require('./api/nieuwsitem'));
   //Nog in commentaar, deze geeft een fout bij het serven
   // app.use('/api/categories', require('./api/categorie'));
+  //app.use('/api/emails', require('./api/email'));
 
   app.use('/auth', require('./auth'));
   
