2cb399d30c7bb7a129615bc783403486d676f1d5
Maxim Geerinck <c4d3r@hotmail.com>
2014-11-12 11:49:55 +0100

    Verdere progressie admin panel

---
+++ admin/src/groep09/AdminBundle/Entity/Email.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by IntelliJ IDEA.
+ * User: Maxim
+ * Date: 12/11/2014
+ * Time: 08:55
+ */
+
+namespace Groep09\AdminBundle\Entity;
+
+
+class Email {
+
+} 
---
+++ admin/src/groep09/AdminBundle/Groep09AdminBundle.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by IntelliJ IDEA.
+ * User: Maxim
+ * Date: 12/11/2014
+ * Time: 08:52
+ */
+
+namespace Groep09\AdminBundle;
+
+
+class Groep09AdminBundle {
+
+} 
